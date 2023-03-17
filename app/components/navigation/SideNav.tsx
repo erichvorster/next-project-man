e62cd0f3704edcfec3e90fe40e1a096361d084c5
erichvorster <erichvorster1@gmail.com>
2023-03-17 09:16:48 +0200

    added nav

---
+++ app/components/navigation/SideNav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <div className="bg-neutral-900 w-12 h-full absolute">
+      <div>
+        <ul className="h-72 flex flex-col justify-between">
+          <li className="mt-4">Logo</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
